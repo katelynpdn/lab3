0b5c4ebcd2d0f083f264861c6a4e699d0f8a0e52
katelynpdn <94756838+katelynpdn@users.noreply.github.com>
2024-02-01 16:46:37 -0800

    Debug reverse

--- ArrayTests.java
+++ ArrayTests.java
@@ -8,11 +8,22 @@ public class ArrayTests {
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
-
+  @Test 
+	public void testReverseInPlace2() {
+    int[] intArray = {3, 8, 9};
+    ArrayExamples.reverseInPlace(intArray);
+    assertArrayEquals(new int[]{9, 8, 3}, intArray);
+	}
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversed2() {
+    int[] intArray = {3, 8, 9 };
+    assertArrayEquals(new int[]{9, 8, 3 }, ArrayExamples.reversed(intArray));
   }
 }
